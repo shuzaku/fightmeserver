2ab739630dd391b07e79605e29e5ca25d38485ec
Marvin Chau <mchau@glasshive.com>
2025-11-24 00:59:19 -0600

    added new endpoints

--- routes/games.js
+++ routes/games.js
@@ -6,6 +6,13 @@ const gameController = require('../controller/games');
 router.post('/games', gameController.addGame);
 router.get('/gameQuery', gameController.queryGame);
 router.get('/games', gameController.getGames);
+
+// Test route to verify routing works
+router.get('/games/test-stats', (req, res) => {
+    res.send({ message: 'Stats route is working!' });
+});
+
+router.get('/games/:id/stats', gameController.getGameStats);
 router.get('/games/:id', gameController.getGame);
 router.put('/games/:id', gameController.updateGame);
 router.delete('/games/:id', gameController.deleteGame);
